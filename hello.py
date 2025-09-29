da0200bd09bc0d122f5ad3e6a7219640869c7b72
Christian <christianbagrin789@gmail.com>
2025-09-29 02:43:41 +0100

    Add contact page with email and link from homepage

--- hello.py
+++ hello.py
@@ -4,8 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<p>Hello, World! I am a Flask app! Visit the <a href="/about">About</a> page.</p>'
+    return """<h1>Home Page</h1>
+        <a href="/about">About</a> |
+        <a href="/contact">Contact</a> 
+	<p>Hello, World! I am a Flask app!.</p>"""
 
 @app.route("/about")
 def about():
-    return '<p>This app is running on the Flask web framework. Read more at <a href="https://flask.palletsprojects.com/">Flask Documentation</a>.</p>'
+    return """h1>About Page</h1>
+	    <p>This app is running on the Flask web framework. Read more at <a href="https://flask.palletsprojects.com/">Flask Documentation</a>.</p>
+<a href="/">Home</a>""
+
+@app.route("/contact")
+def contact():
+    return """
+        <h1>Contact Page</h1>
+        <p>Email: c12345678@mytudublin.ie</p>
+        <a href="/">Home</a>
+    """
